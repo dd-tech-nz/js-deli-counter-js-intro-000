14c0180c74177f9ee23257e5e512fdb41aad09a0
David Dumolo <ddumolo@hotmail.com>
2020-03-13 09:12:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+//cont katzDeliLine = []
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  let number = katzDeliLine.indexOf(name) + 1;
+  return `Welcome, ${name}. You are number ${number} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  let serving = [...katzDeliLine]
+  if (!serving[0]){
+    return "There is nobody waiting to be served!"
+  } else {
+    katzDeliLine.shift()
+    return `Currently serving ${serving[0]}.`
+  }
